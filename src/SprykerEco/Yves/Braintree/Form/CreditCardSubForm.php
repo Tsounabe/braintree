431aad8d3610f866f5d1374cbac97b9b145c06e8
Arnaud Benatsou <arnaud.benatsou@scnd.com>
2025-03-25 12:05:44 +0100

    CS

--- src/SprykerEco/Yves/Braintree/Form/CreditCardSubForm.php
+++ src/SprykerEco/Yves/Braintree/Form/CreditCardSubForm.php
@@ -149,28 +149,26 @@ class CreditCardSubForm extends AbstractSubForm
 
             $billingAddressTransfer = $quoteTransfer->getBillingAddressOrFail();
 
-            $view->vars[static::EMAIL] = $quoteTransfer->getCustomerOrFail()?->getEmail();
+            $view->vars[static::EMAIL] = $quoteTransfer->getCustomerOrFail()->getEmail();
             $view->vars[static::AMOUNT] = $quoteTransfer->getTotals()?->getGrandTotal();
 
-            if ($billingAddressTransfer !== null) {
-                $view->vars[static::BILLING_ADDRESS] = [
-                    static::BILLING_ADDRESS_GIVEN_NAME => $this->convertToGermanAsciiFormat(
-                        $billingAddressTransfer->getFirstNameOrFail()
-                    ),
-                    static::BILLING_ADDRESS_SURNAME => $this->convertToGermanAsciiFormat(
-                        $billingAddressTransfer->getLastNameOrFail()
-                    ),
-                    static::BILLING_ADDRESS_PHONE_NUMBER => $billingAddressTransfer->getPhone(),
-                    static::BILLING_ADDRESS_STREET_ADDRESS => $billingAddressTransfer->getAddress1(),
-                    static::BILLING_ADDRESS_EXTENDED_ADDRESS => $billingAddressTransfer->getAddress2(),
-                    static::BILLING_ADDRESS_LOCALITY => $billingAddressTransfer->getCountry(
-                    ) ? $billingAddressTransfer->getCountry()->getName() : '',
-                    static::BILLING_ADDRESS_REGION => $billingAddressTransfer->getRegion(),
-                    static::BILLING_ADDRESS_POSTAL_CODE => $billingAddressTransfer->getZipCode(),
-                    static::BILLING_ADDRESS_COUNTRY_CODE => $billingAddressTransfer->getCountry(
-                    ) ? $billingAddressTransfer->getCountry()->getIso2Code() : '',
-                ];
-            }
+            $view->vars[static::BILLING_ADDRESS] = [
+                static::BILLING_ADDRESS_GIVEN_NAME => $this->convertToGermanAsciiFormat(
+                    $billingAddressTransfer->getFirstNameOrFail()
+                ),
+                static::BILLING_ADDRESS_SURNAME => $this->convertToGermanAsciiFormat(
+                    $billingAddressTransfer->getLastNameOrFail()
+                ),
+                static::BILLING_ADDRESS_PHONE_NUMBER => $billingAddressTransfer->getPhone(),
+                static::BILLING_ADDRESS_STREET_ADDRESS => $billingAddressTransfer->getAddress1(),
+                static::BILLING_ADDRESS_EXTENDED_ADDRESS => $billingAddressTransfer->getAddress2(),
+                static::BILLING_ADDRESS_LOCALITY => $billingAddressTransfer->getCountry(
+                ) ? $billingAddressTransfer->getCountry()->getName() : '',
+                static::BILLING_ADDRESS_REGION => $billingAddressTransfer->getRegion(),
+                static::BILLING_ADDRESS_POSTAL_CODE => $billingAddressTransfer->getZipCode(),
+                static::BILLING_ADDRESS_COUNTRY_CODE => $billingAddressTransfer->getCountry(
+                ) ? $billingAddressTransfer->getCountry()->getIso2Code() : '',
+            ];
         }
     }
 
